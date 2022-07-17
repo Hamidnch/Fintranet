c16591943ca20203af27d4d0dfa9b2c909ecd320
Hamidnch <Hamidnch2007@gmail.com>
2022-07-18 00:23:46 +0430

    Added GetDoctorByIdQuery.

---
+++ Core/3-Fintranet.Application/Features/Doctors/Queries/GetDoctorByIdQuery.cs
@@ -0,0 +1,24 @@
+﻿using _3_Fintranet.Application.Features.Doctors.Dtos;
+using _3_Fintranet.Application.Features.Doctors.Services;
+using MediatR;
+
+namespace _3_Fintranet.Application.Features.Doctors.Queries
+{
+    public record GetDoctorByIdQuery(Guid Id) : IRequest<DoctorDto<Guid>>
+    {
+        public class GetDoctorByIdQueryHandler : IRequestHandler<GetDoctorByIdQuery, DoctorDto<Guid>>
+        {
+            private readonly IDoctorManager<Guid> _doctorManager;
+
+            public GetDoctorByIdQueryHandler(IDoctorManager<Guid> doctorManager)
+            {
+                _doctorManager = doctorManager;
+            }
+
+            public async Task<DoctorDto<Guid>> Handle(GetDoctorByIdQuery query, CancellationToken cancellationToken)
+            {
+                return await _doctorManager.GetByIdAsync(query.Id);
+            }
+        }
+    }
+}
