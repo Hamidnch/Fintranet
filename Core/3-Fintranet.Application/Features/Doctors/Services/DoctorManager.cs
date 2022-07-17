2998d71d7b4fca37b068b95706aa9996600c9c1e
hamidnch2007 <hamidnch2007@gmail.com>
2022-07-17 17:08:39 +0430

    Added IDoctorManager and DoctorManager

---
+++ Core/3-Fintranet.Application/Features/Doctors/Services/DoctorManager.cs
@@ -0,0 +1,33 @@
+﻿using _2_Fintranet.Domain.Entities;
+using _3_Fintranet.Application.Features.Doctors.Dtos;
+
+namespace _3_Fintranet.Application.Features.Doctors.Services
+{
+    public class DoctorManager : IDoctorManager
+    {
+        public async Task<ResponseDoctorDto> GetAllAsync(RequestDoctorDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DoctorDto> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DoctorDto> CreateAsync(Doctor doctor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DoctorDto> UpdateAsync(Doctor doctor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Guid doctorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/3-Fintranet.Application/Features/Doctors/Services/IDoctorManager.cs
@@ -0,0 +1,12 @@
+﻿using _2_Fintranet.Domain.Entities;
+using _3_Fintranet.Application.Features.Doctors.Dtos;
+namespace _3_Fintranet.Application.Features.Doctors.Services;
+
+public interface IDoctorManager
+{
+    Task<ResponseDoctorDto> GetAllAsync(RequestDoctorDto dto);
+    Task<DoctorDto> GetByIdAsync(Guid id);
+    Task<DoctorDto> CreateAsync(Doctor doctor);
+    Task<DoctorDto> UpdateAsync(Doctor doctor);
+    Task DeleteAsync(Guid doctorId);
+}
