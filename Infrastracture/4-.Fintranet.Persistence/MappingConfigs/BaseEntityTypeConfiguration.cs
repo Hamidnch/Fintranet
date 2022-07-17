eedb786bf5cae512eb83acd9d185a35f406e9ddf
hamidnch2007 <hamidnch2007@gmail.com>
2022-07-17 15:42:36 +0430

    BaseEntityTypeConfiguration Added.

---
+++ Infrastracture/4-.Fintranet.Persistence/MappingConfigs/BaseEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using _2_Fintranet.Domain.Commons;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _4_.Fintranet.Persistence.MappingConfigs
+{
+    public class BaseEntityTypeConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.RowVersion).IsRowVersion();
+            builder.Property(x => x.CreatedDateTime).HasDefaultValueSql("getdate()");
+            builder.Property(x => x.UpdatedDateTime).HasDefaultValueSql("getdate()");
+        }
+    }
+}
