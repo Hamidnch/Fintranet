c24aa117d4bc874627677df093b338218cd09c7a
Hamidnch <Hamidnch2007@gmail.com>
2022-07-18 00:05:32 +0430

    Added CreateDoctorEvent and EmailNotification.

---
+++ Core/3-Fintranet.Application/Features/Doctors/Notifications/CreateDoctorEvent.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace _3_Fintranet.Application.Features.Doctors.Notifications
+{
+    public class CreateDoctorEvent : INotification
+    {
+        public Guid Id { get; set; }
+        public CreateDoctorEvent(Guid id)
+        {
+            Id = id;
+        }
+
+        public class CreateDoctorEmailSenderHandler : INotificationHandler<CreateDoctorEvent>
+        {
+            public Task Handle(CreateDoctorEvent notification, CancellationToken cancellationToken)
+            {
+                // IMessageSender.Send($"Welcome {notification.FirstName} {notification.LastName} !");
+                return Task.CompletedTask;
+            }
+        }
+
+        public class CreateDoctorLoggerHandler : INotificationHandler<CreateDoctorEvent>
+        {
+            private readonly ILogger<CreateDoctorLoggerHandler> _logger;
+
+            public CreateDoctorLoggerHandler(ILogger<CreateDoctorLoggerHandler> logger)
+            {
+                _logger = logger;
+            }
+
+            public Task Handle(CreateDoctorEvent notification, CancellationToken cancellationToken)
+            {
+                _logger.LogInformation($"New doctor has been created with Id: {notification.Id}");
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ Core/3-Fintranet.Application/Features/Doctors/Notifications/EmailNotification.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace _3_Fintranet.Application.Features.Doctors.Notifications
+{
+    public class EmailNotification : INotification
+    {
+        public string EmailAddress { get; }
+        public string EmailContent { get; }
+
+        public EmailNotification(string emailAddress, string emailContent)
+        {
+            EmailAddress = emailAddress;
+            EmailContent = emailContent;
+        }
+
+        public class EmailNotificationHandler : INotificationHandler<EmailNotification>
+        {
+            private readonly ILogger<EmailNotificationHandler> _logger;
+
+            public EmailNotificationHandler(ILogger<EmailNotificationHandler> logger)
+            {
+                _logger = logger;
+            }
+
+            public Task Handle(EmailNotification notification, CancellationToken cancellationToken)
+            {
+                _logger.LogWarning("Sending Email to {EmailAddress} with content {EmailContent}",
+                    notification.EmailAddress, notification.EmailContent);
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
