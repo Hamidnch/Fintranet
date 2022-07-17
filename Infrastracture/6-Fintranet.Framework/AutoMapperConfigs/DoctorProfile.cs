83e3f1c058a1aa8b1897dc30471a0a05090d9cd8
Hamidnch <Hamidnch2007@gmail.com>
2022-07-18 00:06:04 +0430

    Added DoctorProfile for AutoMapper.

---
+++ Infrastracture/6-Fintranet.Framework/AutoMapperConfigs/DoctorProfile.cs
@@ -0,0 +1,14 @@
+﻿using _2_Fintranet.Domain.Entities;
+using _3_Fintranet.Application.Features.Doctors.Dtos;
+using AutoMapper;
+
+namespace _6_Fintranet.Framework.AutoMapperConfigs
+{
+    public class DoctorProfile : Profile
+    {
+        public DoctorProfile()
+        {
+            CreateMap<Doctor, DoctorDto<Guid>>().ReverseMap();
+        }
+    }
+}
