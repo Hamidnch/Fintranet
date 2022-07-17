24c39c4ceb5bf7abcef71ded9f34c736d5a8a652
Hamidnch <Hamidnch2007@gmail.com>
2022-07-18 00:06:39 +0430

    Added LoggerBehavior and ValidatorBehavior.

---
+++ Core/3-Fintranet.Application/Features/Doctors/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace _3_Fintranet.Application.Features.Doctors.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse> where TResponse : class where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                //pre
+                _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+                //next
+                TResponse response = await next();
+                //post
+                _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occurred!!");
+                _logger.LogError(e, e.Message);
+                return default!;
+            }
+        }
+    }
+}
---
+++ Core/3-Fintranet.Application/Features/Doctors/Behaviors/ValidatorBehavior.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using System.Text;
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+
+namespace _3_Fintranet.Application.Features.Doctors.Behaviors
+{
+    public class ValidatorBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidatorBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, 
+            CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            
+            ValidationResult[] validationResults = await Task.WhenAll(
+                _validators.Select(v => 
+                    v.ValidateAsync(context, cancellationToken)));
+
+            List<ValidationFailure> failures = validationResults
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .ToList();
+
+            if (!failures.Any()) return await next();
+
+            var errorBuilder = new StringBuilder();
+            errorBuilder.AppendLine("Invalid command, reason: ");
+            foreach (var error in failures)
+            {
+                errorBuilder.AppendLine(error.ErrorMessage);
+            }
+
+            throw new ValidationException(failures);
+        }
+    }
+}
