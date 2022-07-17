a1e1e968dfa6a2397ec92d8c55bdfb0ae088b9fa
hamidnch2007 <hamidnch2007@gmail.com>
2022-07-17 15:25:31 +0430

    FintranetContextFactory Added.

--- Infrastracture/4-.Fintranet.Persistence/Contexts/FintranetContextFactory.cs
+++ Infrastracture/4-.Fintranet.Persistence/Contexts/FintranetContextFactory.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace _4_.Fintranet.Persistence.Contexts
 {
-    internal class FintranetContextFactory
+    public class FintranetContextFactory
     {
+        public FintranetContext CreateDbContext(string args)
+        {
+            var basePath = Directory.GetCurrentDirectory();
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(basePath: basePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString =
+                configBuilder.GetConnectionString("DefaultConnectionString");
+            var builder = new DbContextOptionsBuilder<FintranetContext>();
+            builder.UseSqlServer(connectionString);
+            return new FintranetContext(builder.Options);
+        }
     }
 }
