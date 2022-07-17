e0065226fdf21280ceb1302cc497b816d1d8a19a
hamidnch2007 <hamidnch2007@gmail.com>
2022-07-17 16:42:03 +0430

    Added PaginationExtensions

---
+++ Core/1-Fintranet.Common/Extensions/PaginationExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace _1_Fintranet.Common.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static IEnumerable<T> ToPaged<T>(this IEnumerable<T> source, int page, int pageSize)
+        {
+            var enumerable = source as T[] ?? source.ToArray();
+            return enumerable.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IEnumerable<T> ToPaged<T>(this IEnumerable<T> source, int page, int pageSize, out int rowCount)
+        {
+            var enumerable = source as T[] ?? source.ToArray();
+            rowCount = enumerable.Count();
+            return enumerable.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
