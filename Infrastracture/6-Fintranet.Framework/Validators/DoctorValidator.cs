e3a8dad56d47ae68c32d9fd41aa53d48f23933ce
Hamidnch <Hamidnch2007@gmail.com>
2022-07-18 00:25:19 +0430

    Added DoctorValidator in Fintranet.Framework project.

---
+++ Infrastracture/6-Fintranet.Framework/Validators/DoctorValidator.cs
@@ -0,0 +1,19 @@
+﻿using _1_Fintranet.Common.Validators;
+using _2_Fintranet.Domain.Entities;
+using FluentValidation;
+
+namespace _6_Fintranet.Framework.Validators
+{
+    public class DoctorValidator : AbstractValidator<Doctor>
+    {
+        public DoctorValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email must not be empty");
+            RuleFor(x => x.Email)
+                .Must(EmailValidator.Validate!)
+                .WithMessage("Email is invalid.");
+        }
+    }
+}
